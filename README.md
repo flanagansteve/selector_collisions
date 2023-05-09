41d8f436ac52e271269d036f9f16d3599f7adbaf
Steve Flanagan <flanagan.s@husky.neu.edu>
2023-05-09 10:00:25 -0400

    move max_function_name_len to arg

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ usage:
 
 1. $ python3
 2. \> from get_selector_collision import *
-3. \> get_selector_collision('functionName(argType1,argType2)')
+3. \> get_selector_collision(signature: 'functionName(argType1,argType2)', max_function_name_length: 1)
--- get_selector_collision.py
+++ get_selector_collision.py
@@ -14,10 +14,6 @@ def generate_function(target_selector, args, function_name_len):
   letters = list(string.ascii_letters)
   letters.append('_')
 
-  selector = ''
-  candidate_char_indices = [0] * function_name_len
-  next_candidate_char = 0
-
   for function_name_candidate in product(letters, repeat=function_name_len):
     print('trying: ' + ''.join(function_name_candidate))
     selector = get_selector(''.join(function_name_candidate) + args)
@@ -27,14 +23,13 @@ def generate_function(target_selector, args, function_name_len):
 
   return { 'selector' : 'NOT_FOUND' }
 
-def get_selector_collision(signature):
+def get_selector_collision(signature, max_function_name_len):
   function_name = signature[:signature.index('(')]
   function_args = signature[signature.index('('):]
 
   target_selector = get_selector(signature)
   print('target: ' + target_selector)
 
-  max_function_name_len = 2
   candidate_function_length = 1
 
   returned_function = { 'selector' : 'NOT_FOUND', 'function_signature' : '' }
